e760bc8f300851efd454dc358457aa6bef84f609
stephanegaiffas <stephane.gaiffas@gmail.com>
2020-03-05 23:13:46 +0100

    ...

--- check_speed.py
+++ check_speed.py
@@ -28,11 +28,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, stratify=y, test_size=0.3, random_state=42
 )
 
-X_train = X_train.astype("float32")
-X_test = X_test.astype("float32")
-y_train = y_train.astype("float32")
-y_test = y_test.astype("float32")
-
 # Precompilation
 amf = AMFClassifier(
     n_classes=n_classes,
