093e7679f28c34f76997907a6a9f5846d4d8c9e0
stephanegaiffas <stephane.gaiffas@gmail.com>
2020-03-19 12:30:20 +0100

    Adds playground_decision with several datasets together with several improvements

--- onelearn/plot.py
+++ onelearn/plot.py
@@ -7,23 +7,34 @@ import matplotlib.pyplot as plt
 # TODO: docstrings
 
 
-def get_mesh(X, h=0.02):
+def get_mesh(X, h=0.02, padding=0.5):
     """Build a regular meshgrid using the range of the features in X
     """
-    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+    x_min, x_max = X[:, 0].min() - padding, X[:, 0].max() + padding
+    y_min, y_max = X[:, 1].min() - padding, X[:, 1].max() + padding
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     X_mesh = np.array([xx.ravel(), yy.ravel()]).T
     return xx, yy, X_mesh
 
 
 def plot_scatter_binary_classif(
-    ax, xx, yy, X, y, s=10, alpha=None, cm=None, title=None, fontsize=None
+    ax,
+    xx,
+    yy,
+    X,
+    y,
+    s=10,
+    alpha=None,
+    cm=None,
+    title=None,
+    fontsize=None,
+    lw=None,
+    norm=None,
 ):
     if cm is None:
         cm = plt.get_cmap("RdBu")
 
-    ax.scatter(X[:, 0], X[:, 1], c=y, s=s, cmap=cm, alpha=alpha)
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=s, cmap=cm, alpha=alpha, lw=lw, norm=norm)
     if title is not None:
         ax.set_title(title, fontsize=fontsize)
     ax.set_xlim(xx.min(), xx.max())
@@ -33,11 +44,11 @@ def plot_scatter_binary_classif(
 
 
 def plot_contour_binary_classif(
-    ax, xx, yy, Z, cm=None, alpha=0.8, levels=200, title=None, score=None
+    ax, xx, yy, Z, cm=None, alpha=0.8, levels=200, title=None, score=None, norm=None
 ):
     if cm is None:
         cm = plt.get_cmap("RdBu")
-    ax.contourf(xx, yy, Z, cmap=cm, alpha=alpha, levels=levels)
+    ax.contourf(xx, yy, Z, cmap=cm, alpha=alpha, levels=levels, norm=norm)
     ax.set_xlim(xx.min(), xx.max())
     ax.set_ylim(yy.min(), yy.max())
     ax.set_xticks(())
