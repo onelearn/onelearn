60e5f042fd98aae68fdde06b9618be5753f4d915
stephanegaiffas <stephane.gaiffas@gmail.com>
2020-04-06 11:56:05 +0200

    Version 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = "2020, Stéphane Gaïffas"
 author = "Stéphane Gaïffas"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.5"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
